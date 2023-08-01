b08d317b0335cf54ba686175414589231aded4a3
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-08-01 09:01:03 +0530

    updated cart page as per NaveenTL review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,9 +1,14 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
-	String product = "macbook";
+	By product = By.id("productId");
 	
+	public void clickproduct() {
+		System.out.println("click on macbook");
+	}
 	
 	
 
