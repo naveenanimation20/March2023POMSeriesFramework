b138217f007d7efb4f63387f78a6b4fc0616a334
Naveen AutomationLabs <naveenanimation20@gmail.com>
2023-08-03 13:09:34 +0530

    added screenshot folder

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -138,6 +138,8 @@ public class DriverFactory {
 	public static String getScreenshot(String methodName) {
 		File srcFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
 		String path = System.getProperty("user.dir")+"/screenshot/"+methodName+"_"+System.currentTimeMillis()+".png";
+		System.out.println("user directory: " + System.getProperty("user.dir"));
+		System.out.println("screenshot path: " + path);
 		File destination = new File(path);
 		try {
 			FileHandler.copy(srcFile, destination);
